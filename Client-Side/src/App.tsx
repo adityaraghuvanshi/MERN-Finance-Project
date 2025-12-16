dd2ca68dd261231e85a02bd26517ebcb1d845bcd
Aditya Raghuvanshi <raghuvanshiaditya2211@gmai.com>
2025-12-16 17:34:58 +0530

    Added fonts

--- Client-Side/src/App.tsx
+++ Client-Side/src/App.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [themeMode, setThemeMode] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', themeMode);
+    localStorage.setItem("theme", themeMode);
+  }, [themeMode])
+
+  const handleClick = () => {
+    setThemeMode(prev => prev === "light" ? "dark" : "light")
+  }
 
   return (
     <>
       <div className='homepage'>
-      HOMEPAGE
+        <button onClick={handleClick}>Switch</button>
+        
       </div>
-      
+
     </>
   )
 }
